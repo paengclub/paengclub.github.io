3a4ae4fe6e27200f2c19f5bd6e567d5cd9e8c0e9
Jiwon <57394364+JiwonPaeng@users.noreply.github.com>
2023-06-11 04:25:35 +0000

    fixed #13 & implemented #6

---
+++ data.js
@@ -0,0 +1,24 @@
+const ITINERARY = [
+    {name:'예지민', type:'특별 외박', date:'2023-06-09'},
+    {name:'예지민', type:'외박 복귀', date:'2023-06-11'},
+    {name:'예지민', type:'특별 외박', date:'2023-06-16'},
+    {name:'팽지원', type:'분기 외박', date:'2023-06-17'},
+    {name:'예지민', type:'외박 복귀', date:'2023-06-18'},
+    {name:'팽지원', type:'외박 복귀', date:'2023-06-18'},
+    {name:'예지민', type:'특별 외박', date:'2023-06-23'},
+    {name:'예지민', type:'외박 복귀', date:'2023-06-25'},
+    {name:'예지민', type:'한국 출국', date:'2023-07-26'},
+    {name:'심우재', type:'유격 훈련', date:'2023-07-31'}
+];
+
+const DATA = [
+    {name:'허채민', rank:'PFC', enlisted:'2022-10-04', discharged:'2024-04-03'},
+    {name:'예지민', rank:'PFC', enlisted:'2023-01-30', discharged:'2024-07-29'},
+    {name:'팽지원', rank:'PFC', enlisted:'2023-03-20', discharged:'2024-09-19'},
+    {name:'심우재', rank:'PV2', enlisted:'2023-04-03', discharged:'2024-10-02'},
+    {name:'최재우', rank:'PV2', enlisted:'2023-04-10', discharged:'2024-10-09'},
+    {name:'이승우', rank:'PV2', enlisted:'2023-04-24', discharged:'2025-01-23'},
+    {name:'이성민', rank:'GEN', enlisted:'2023-07-10', discharged:'2025-04-09'}
+];
+
+export {ITINERARY, DATA};
