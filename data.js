6dc9a528bd2201b6a508a733ab380678346d0a10
Jiwon <57394364+JiwonPaeng@users.noreply.github.com>
2024-04-14 16:17:05 +0900

    added our staff sergeant

--- data.js
+++ data.js
@@ -1,13 +1,14 @@
 let ITINERARY = [
     {name:'예지민', type:'한국 귀국', date:'2024-04-15'},
     {name:'김현서', type:'훈련소 수료', date:'2024-09-12'},
-    {name:'팽지원', type:'말출', date:'2024-08-19'}
+    {name:'팽지원', type:'말출', date:'2024-08-19'},
+    {name:'심우재', type:'하사 진급', date:'2024-10-02'}
 ];
 
 let DATA = [
     {name:'예지민', rank:'PFC.svg', enlisted:'2023-01-30', PFC:'2023-04-01', CPL:'2023-10-01', SGT:'2024-04-01', discharged:'2024-07-29', ANF:'육군'},
     {name:'팽지원', rank:'PFC.svg', enlisted:'2023-03-20', PFC:'2023-06-01', CPL:'2023-12-01', SGT:'2024-06-01', discharged:'2024-09-19', ANF:'육군'},
-    {name:'심우재', rank:'PV2.svg', enlisted:'2023-04-03', PFC:'2023-07-01', CPL:'2024-01-01', SGT:'2024-07-01', discharged:'2024-10-02', ANF:'육군'},
+    {name:'심우재', rank:'PV2.svg', enlisted:'2023-04-03', PFC:'2023-07-01', CPL:'2024-01-01', SGT:'2024-07-01', discharged:'2025-04-02', ANF:'육군'},
     {name:'최재우', rank:'PV2.svg', enlisted:'2023-04-10', PFC:'2023-07-01', CPL:'2024-01-01', SGT:'2024-07-01', discharged:'2024-10-09', ANF:'육군'},
     {name:'이승우', rank:'PV2.svg', enlisted:'2023-04-24', PFC:'2023-06-24', CPL:'2023-12-24', SGT:'2024-06-24', discharged:'2025-01-23', ANF:'공군'},
     {name:'이성민', rank:'GEN.svg', enlisted:'2023-07-10', PFC:'2023-09-10', CPL:'2024-03-10', SGT:'2024-09-10', discharged:'2025-04-09', ANF:'공군'},
